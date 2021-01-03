766541258b46be5e31f7f7b0614f6c318eff2f40
Keiana Hanson <kiki_hanson@aol.com>
2021-01-02 21:53:03 -0500

    set up connection to DB, connected successfully to MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+// Dependencies
 const express = require("express");
+const connectDB = require('./config/db');
 
 const app = express();
+//if there is no environmental port, than run port 5000 locally
+const PORT = process.env.PORT || 5000;
 
+// Connect to Database
+connectDB();
+
+
+// ** ROUTES **
 // sends data to the browser, 
 // and when hitting /, i.e http://localhost:5000, it will display the below msg
 app.get('/', (req,res) => res.send('API Running'));
 
-//if there is no environmental port, than run port 5000 locally
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
